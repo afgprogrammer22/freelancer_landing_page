7159ec5f7b776f3d46ed4f42c513bec42a5bdcf7
Aziz Mohammadi <68205086+iamazizmohmdi@users.noreply.github.com>
2022-04-09 09:40:32 +0530

    changed header name into navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import { Header } from './components/index.js';
+import { Navbar } from './components/index.js';
 
 const App = () => {
   return (
     <div>
-      <Header />
+      <Navbar />
     </div>
   )
 }
